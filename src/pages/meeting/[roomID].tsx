964af4ef0de28bd80ae1f6e969a453e96462ab23
mohammed5420 <almoaz065@gmail.com>
2022-08-28 22:05:56 +0200

    remove room data

--- src/pages/meeting/[roomID].tsx
+++ src/pages/meeting/[roomID].tsx
@@ -7,47 +7,7 @@ import { useEffect } from "react";
 import Navbar from "../../components/Navbar";
 const AgoraMain = dynamic(import("../../components/AgoraMain"), { ssr: false });
 
-type Room = {
-  id: string;
-  hostID: string;
-};
-
-export async function getStaticPaths() {
-  const result = await fetch(`${process.env.APP_URL}/api/room`);
-  const data = await result.json();
-  if (data?.message) {
-    return;
-  }
-  const paths = await data.map((room: Room) => {
-    return { params: { roomID: room.id.toString() } };
-  });
-  return {
-    paths,
-    fallback: false,
-  };
-}
-
-type Props = {
-  roomInfo: {
-    id: string;
-    hostID: string;
-  };
-};
-
-export const getStaticProps: GetStaticProps = async ({ params }) => {
-  if (!params) return { props: {} };
-  const result = await fetch(
-    `${process.env.APP_URL}/api/room/${params.roomID}`
-  );
-  const roomInfo = await result.json();
-  return {
-    props: {
-      roomInfo,
-    },
-  };
-};
-
-const MeetingRoom: NextPage<Props> = () => {
+const MeetingRoom: NextPage = () => {
   const session = useSession();
   useEffect(() => {
     //Redirect the user to the login page
